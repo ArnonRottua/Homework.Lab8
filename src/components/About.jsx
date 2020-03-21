2b49effc726bc5e69c5a2ead3f192123cf2fe4dd
ArnonRottua <58945762+ArnonRottua@users.noreply.github.com>
2020-03-21 14:22:24 +0700

    Homework.Lab8

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,10 +6,10 @@ export default function About() {
     <div>
       <div className="container mt-5">
         <div className="card">
-          <img src={process.env.PUBLIC_URL+'/andreas-klassen.jpg'} className='card-img-top' alt="..." />
+          <img src={'https://firebasestorage.googleapis.com/v0/b/lab8-caaf0.appspot.com/o/Cartoon.jpg?alt=media&token=01a517be-b163-49b9-9869-e98923873e81'} className='card-img-top' alt="..." />
           <div className="card-body">
             <h5 className="card-title">Developer information</h5>
-            <p>Dome Potikanond (610610999)</p>
+            <p>Arnon Rottua (610610625)</p>
             <p>This app use Google Firebase as backend.</p>
             <Link to="/">
               <h3>
